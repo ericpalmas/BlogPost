b7670fd99d7da2b5850baacadceb1077fa893236
ERIC PALMAS <eric.palmas@student.supsi.ch>
2018-10-06 16:26:44 +0200

    prova

--- src/main/java/ch/supsi/webapp/HelloServlet.java
+++ src/main/java/ch/supsi/webapp/HelloServlet.java
@@ -1,6 +1,10 @@
 package ch.supsi.webapp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,13 +12,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(value="/hello")
+@WebServlet(value="/blogpost")
 @SuppressWarnings("serial")
 public class HelloServlet extends HttpServlet {
+	ArrayList<BlogPost> blogPosts = new ArrayList<>();
+	ObjectMapper mapper = new ObjectMapper();
 
+	String getValue(String title, String text, String author) throws JsonProcessingException {
+		blogPosts.add(new BlogPost(title,text, author));
+		return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(blogPosts);
+	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		res.getWriter().println("Hello world!!");
+		//res.getWriter().println("get request effettuata \n\n"+ getValue(req.getParameter("title"), req.getParameter("text"),req.getParameter("author")));
+		mapper.writerWithDefaultPrettyPrinter().writeValueAsString(blogPosts);
 	}
-    
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		//res.getWriter().println("post request effettuata\n\n"+ getValue(req.getParameter("title"), req.getParameter("text"),req.getParameter("author")));
+		BlogPost blogPost = new BlogPost(req.getParameter("title"),req.getParameter("text"),req.getParameter("author"));
+		blogPosts.add(blogPost);
+		mapper.writerWithDefaultPrettyPrinter().writeValueAsString(blogPost);
+	}
 }
