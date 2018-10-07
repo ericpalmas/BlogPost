a9cf6d257e4cceee271cf1c78cb083bab7f3def1
ERIC PALMAS <eric.palmas@student.supsi.ch>
2018-10-07 15:56:05 +0200

    prova

--- src/main/java/ch/supsi/webapp/HelloServlet.java
+++ src/main/java/ch/supsi/webapp/HelloServlet.java
@@ -1,11 +1,8 @@
 package ch.supsi.webapp;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,20 +15,14 @@ public class HelloServlet extends HttpServlet {
 	ArrayList<BlogPost> blogPosts = new ArrayList<>();
 	ObjectMapper mapper = new ObjectMapper();
 
-	String getValue(String title, String text, String author) throws JsonProcessingException {
-		blogPosts.add(new BlogPost(title,text, author));
-		return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(blogPosts);
-	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		//res.getWriter().println("get request effettuata \n\n"+ getValue(req.getParameter("title"), req.getParameter("text"),req.getParameter("author")));
-		mapper.writerWithDefaultPrettyPrinter().writeValueAsString(blogPosts);
+		res.getWriter().println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(blogPosts));
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		//res.getWriter().println("post request effettuata\n\n"+ getValue(req.getParameter("title"), req.getParameter("text"),req.getParameter("author")));
-		BlogPost blogPost = new BlogPost(req.getParameter("title"),req.getParameter("text"),req.getParameter("author"));
+		BlogPost blogPost = mapper.readValue(req.getInputStream(), BlogPost.class);
 		blogPosts.add(blogPost);
-		mapper.writerWithDefaultPrettyPrinter().writeValueAsString(blogPost);
+		res.getWriter().println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(blogPost));
 	}
 }
